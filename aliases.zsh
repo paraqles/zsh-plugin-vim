02c4c6a4612a8aae4485dc7879aebc4e2182466d
paraqles <paraqles@dthal.net>
2015-02-17 17:06:25 +0100

    added aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,4 @@
-for al in `ls $ZSH_PLUGIN_VIM_DIR/aliases.*.zsh`; do
+for al in `ls aliases.*.zsh`; do
   source $al
 done
 
--- vim.zsh
+++
@@ -1,3 +0,0 @@
-ZSH_PLUGIN_VIM_DIR=$ZSH_PLUGINS_DIR/vim
-
-source $ZSH_PLUGIN_VIM_DIR/aliases.zsh
